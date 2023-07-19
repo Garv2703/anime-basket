6ab1769b15d2ca4ae91d7c40d98c704df2c651a9
garv <garvs0109@gmail.com>
2023-07-19 17:34:27 +0530

    Added a new template

--- animebasketshop/views.py
+++ animebasketshop/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
 
 # Create your views here.
 def home(request):
     template = loader.get_template('home.html')
-    context = {}
+    context = {
+        'media_url': 'https://' if request.is_secure() else 'http://' + request.META['HTTP_HOST'] + settings.MEDIA_URL,
+    }
     return HttpResponse(template.render(context, request))
