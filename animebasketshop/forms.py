fe052d131de7374b3ae393b9ba50c929666aafa8
garv <garvs0109@gmail.com>
2023-07-21 18:05:23 +0530

    Added signup option

---
+++ animebasketshop/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import TextInput, EmailInput, PasswordInput
+
+# Create your forms here.
+class SignupForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email Address'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
