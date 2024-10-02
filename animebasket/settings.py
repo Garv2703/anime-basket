2a9638af541b8b83e7b4596004be9fa5d54fef8c
garv <garvs0109@gmail.com>
2024-10-02 15:39:28 +0530

    checking

--- animebasket/settings.py
+++ animebasket/settings.py
@@ -24,7 +24,7 @@ API_URL = 'https://animebasket-api.vercel.app/anime/gogoanime/'
 SECRET_KEY = 'django-insecure-00z*=ra+ku-^qw=*^5ser^h5+%sh0yb8&*^p3eou)tntz4f)oz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
